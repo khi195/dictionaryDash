25b5639bdc642f4aee4f392460d662b5aa172435
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-10-15 16:29:03 +0100

    example permutation file

--- permutation.m
+++ permutation.m
@@ -1,17 +1,17 @@
 function [ Result ] = permutation( Value,Level )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
+%permutation.m 
+%calculate the number of permutations in the array
 if nargin < 2
  Level = 1;
 end
 if Value > 1
- fprintf('Value = %d Level = %dn', Value, Level);
- Result = permutation(Value - 1, Level + 1) * Value;
+ fprintf('Value = %d Level = %d \n', Value, Level);
+ Result = permutation(Value - 1, Level + 1) * Value; % recursive
  disp(['Result = ', num2str(Result)]);
 else
- fprintf('Value = %d Level = %dn', Value, Level);
+ fprintf('Value = %d Level = %d \n', Value, Level);
  Result = 1;
  disp(['Result = ', num2str(Result)]);
-end
+end     
 
 end
