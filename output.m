8cf95e6452edb860b424e0c846533ec87f4c3878
Kashif Iqbal <Kashif.h.iqbal@gmail.com>
2016-10-21 10:58:47 +0100

    output now does not include permutation

--- output.m
+++ output.m
@@ -1,12 +1,12 @@
-function [] = output(dictionary_array, length_of_dictionary )
+function [] = output(dictionary_array, length_of_dictionary, permute_string )
 %output.m
 %Write the output to a file
 
 % open the file
-fileID = fopen('result.txt','w');
+fileID = fopen('result.txt','a');
 
 words = length_of_dictionary + 1;
-
+fprintf(fileID,'\n\n');  
 for i = 1:words
  word_in_chain = dictionary_array{i};   
      if i < words
@@ -17,7 +17,7 @@ for i = 1:words
 end
 fprintf(fileID,'\n\n');   
 fprintf(fileID,'length of dictionary chain: %i',length_of_dictionary);
-
+fprintf(fileID,'\n\n');   
 fclose(fileID);
 
 end
